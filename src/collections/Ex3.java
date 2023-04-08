3b2c037078939757207184ca8121b9cc20a2e190
filipesenaifirjan <filipemattossenai@gmail.com>
2023-04-08 12:40:45 -0300

    ajuste final

--- src/collections/Ex3.java
+++ src/collections/Ex3.java
@@ -1,3 +1,4 @@
+package collections;
 import java.util.*;  
 public class Ex3 {  
     public static void main(String a[]){         
--- src/collections/Ex4.java
+++ src/collections/Ex4.java
@@ -1,3 +1,4 @@
+package collections;
 import java.util.*;  
 class Ex4{  
 public static void main(String args[]){  
