747ec1f235c291755cce46df34997499241a6e4a
filipesenaifirjan <filipemattossenai@gmail.com>
2023-04-08 11:41:21 -0300

    ex3, min

---
+++ src/Ex3.java
@@ -0,0 +1,15 @@
+import java.util.*;  
+public class Ex3 {  
+    public static void main(String a[]){         
+        
+        List<Integer> list = new ArrayList<Integer>();  
+        list.add(46);  
+        list.add(87);  
+        list.add(20);  
+        list.add(164);  
+        list.add(8);  
+        list.add(32);  
+        
+        System.out.println("Nesse exemplo, vou imprimir o valor mínimo da lista: "+Collections.min(list));  
+    }  
+}  
